65b2df5ab52456a9d293d3004b70739f55ebf8db
Andrés Weitzel <andres96energy@hotmail.com>
2023-06-11 13:19:53 -0300

    Changed aws-sdk-v2 to aws-sdk-v3

--- src/lambdas/senderSQS.js
+++ src/lambdas/senderSQS.js
@@ -1,3 +1,6 @@
+//External
+const { SendMessageCommand, SQSClient } = require("@aws-sdk/client-sqs");
+
 //Environment Vars
 const REGION = process.env.AWS_REGION;
 const ACCESS_KEY = process.env.AWS_ACCESS_KEY_RANDOM_VALUE;
@@ -5,35 +8,41 @@ const SECRET_KEY = process.env.AWS_SECRET_KEY_RANDOM_VALUE;
 const ENDPOINT = process.env.SQS_URL;
 const QUEUE_FIFO_ONE_URL = process.env. QUEUE_FIFO_ONE_URL; 
 
-
 module.exports.handler = async (event) => {
-
-  const AWS = require("aws-sdk");
-  
-  const SQS = new AWS.SQS({
-    accessKeyId: ACCESS_KEY,
-    secretAccessKey: SECRET_KEY,
-    endpoint: ENDPOINT
-  });
 
   try {
 
-  // SQS message parameters
-  var queueParams = {
-    //MessageBody: event.body,
-    MessageBody: 'Message for send',
+    
+  const client = new SQSClient({ 
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_KEY,
+    endpoint: ENDPOINT });
+    
+
+  const command = new SendMessageCommand({
     QueueUrl: QUEUE_FIFO_ONE_URL,
+    DelaySeconds: 10,
+    MessageBody:
+      "Information about current NY Times fiction bestseller for week of 12/11/2016.",
     MessageAttributes: {
-        AttributeName: {
-          StringValue: "Attribute Value",
-          DataType: "String",
-        },
+      Title: {
+        DataType: "String",
+        StringValue: "The Whistler",
       },
-  };
+      Author: {
+        DataType: "String",
+        StringValue: "John Grisham",
+      },
+      WeeksOn: {
+        DataType: "Number",
+        StringValue: "6",
+      },
+    }
+  });
 
-    const result = await SQS.sendMessage(queueParams).promise();
 
-    console.log(JSON.stringify(result, null, 2));
+    const response = await client.send(command);
+    console.log(response);
 
   } catch (e) {
     console.error(e);
